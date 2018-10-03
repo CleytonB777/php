ec25efa8595ee13be45be09dc074f0ceaad9350b
CleytonB777 <cleytoncbmmg@gmail.com>
2018-10-03 00:27:29 -0300

    resolvi o erro

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-<<<<<<< HEAD
     echo "Olá mundo 456";
 
     echo "Olá Udemy";
-=======
-    echo "Olá mundo 123";
-
-    echo "Olá HCode";
->>>>>>> 31031be2711063ff6ea1737200753d497766fb79
 
 
 ?>
